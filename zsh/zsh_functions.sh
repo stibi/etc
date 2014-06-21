fd1837aeddd1ce4d33f16639a30b0bcd0a6bf628
Martin Stiborsky <martin.stiborsky@gmail.com>
2014-06-21 23:05:12 +0200

    Aliases and functions moved to separate files

---
+++ zsh/zsh_functions.sh
@@ -0,0 +1,20 @@
+largestCachedPkgs() {
+    local pkgCachePath="/var/cache/pacman/pkg"
+    local printedPkgsCount=10
+    du -ah ${pkgCachePath} | sort -hr | head -n ${printedPkgsCount}
+}
+
+removeLargestCachedPkgs() {
+    local howManyToDelete=$1
+    local defaultNumberOfDeletedPkgs=10
+    # pokud neni zadano kolik, tak default=10
+    # TODO finish the function
+}
+
+# Kudos to: https://github.com/tejr/dotfiles/blob/master/bash/bashrc.d/scr.bash
+#
+# Create a temporary directory and change into it, to stop me putting stray
+# files into $HOME, and making the system do cleanup for me
+giveMeTemp() {
+    pushd -- "$(mktemp -d)"
+}
